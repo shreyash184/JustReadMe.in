641811c63c3a1ba6010282c2b95d97189c3ca364
Shreyash Chavhan <shreyashchavhan@Shreyashs-MacBook-Air.local>
2025-04-30 02:01:12 +0530

    Code: Made the ui look better

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,21 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const Tab1 = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/Tab1', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { 
+	blog: blog,
+	Tab1: Tab1
+};
